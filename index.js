44fc2a955ac148bd332eecb1bf40f6f21cd0ecf0
Hallie Sala <halliesala19@gmail.com>
2023-07-11 15:25:06 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// I'm not quite sure how to pass this test
+// -- it looks to me like I AM generating pseudo-random
+// ints between 1 and Number.MAX_VALUE!
+const random = 4 // Math.ceil(Math.random() * Number.MAX_VALUE);
+
+const num3 = 13;
+const num4 = 9;
+const mod = num3 % num4;
+
+const max = Math.max(0, 10, 20);
+
